2cbb1f98fa7b92f4776286a40722a79897c3a1eb
mPratik09 <pratik.markad1@gmail.com>
2024-08-03 00:19:06 -0700

    first commit for Project_02

--- src/project_02/Test02_01.java
+++ src/project_02/Test02_01.java
@@ -3,5 +3,7 @@ package project_02;
 public class Test02_01 {
 	public static void main(String[] args) {
 		System.out.println("Project 02..");
+		System.out.println("Project 02..");
+		System.out.println("Project 02..");
 	}
 }
